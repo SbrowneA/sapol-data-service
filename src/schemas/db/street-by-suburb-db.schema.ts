98723a11b47a7277bfbb4309d2a486fcea27e036
SbrowneA <samuel.browne.mx@gmail.com>
2026-03-14 22:46:10 +1030

    Adding: street-by-suburb-db.schema.ts and street-by-suburb-table.service.ts

---
+++ src/schemas/db/street-by-suburb-db.schema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const StreetBySuburbDbSchema = z.object({
+  streets_by_suburb_id: z.int(),
+  osm_id: z.int(),
+  street_canon: z.string(),
+  suburb_name: z.string(),
+  suburb_osm_id: z.string(),
+  street_osm_ids: z.array(z.int()),
+  street_geom: z.json()
+});
+
+export type StreetBySuburbDb = z.infer<typeof StreetBySuburbDbSchema>;
