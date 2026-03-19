7498849a9ceb50f6e921adc381cf826979870bae
SbrowneA <samuel.browne.mx@gmail.com>
2026-03-19 15:56:25 +1030

    Adding click handlers for streets on map.js
    * Making map.js IIFE

--- src/public/js/map.js
+++ src/public/js/map.js
@@ -2,53 +2,67 @@
  * Source: https://github.com/maplibre/maplibre-gl-js
  * @type {e.Map|e.Map}
  */
-var map = new maplibregl.Map({
-  container: 'map',
-  // MapLibre GL JS with MapTiler maps: https://docs.maptiler.com/maplibre/
-  style: '/libs/maptiler-streets-v4/style.json',
-  // starting position [lng, lat]
-  center: [138.588, -34.9],
-  // starting zoom (lower=less-zoom, higher=more-zoom)
-  zoom: 10
-});
+(() =>{
+  const map = new maplibregl.Map({
+    container: 'map',
+    // MapLibre GL JS with MapTiler maps: https://docs.maptiler.com/maplibre/
+    style: '/libs/maptiler-streets-v4/style.json',
+    // starting position [lng, lat]
+    center: [138.588, -34.9],
+    // starting zoom (lower=less-zoom, higher=more-zoom)
+    zoom: 10
+  });
 
-map.on('load', async function() {
-  // console.log('Map loaded!');
-  try {
+  map.on('load', async function () {
+    console.log('Map loaded!');
     const { resolved } = await (await fetch('test-db/resolved-location-by-suburb')).json() || {};
-    console.log('resolved', resolved);
-    // locations.resolved.forEach((r) => {
+    try {
+      console.log('resolved', resolved);
 
-    const featureCollection = {
-      type: 'FeatureCollection',
-      features: resolved.map((r) => ({
-        type: 'Feature',
-        properties: {
-          street: r.street,
-          suburb: r.suburb,
-          streetBySuburbId: r.streetBySuburbId
+      const featureCollection = {
+        type: 'FeatureCollection',
+        features: resolved.map((r) => ({
+          type: 'Feature',
+          properties: {
+            street: r.street,
+            suburb: r.suburb,
+            streetBySuburbId: r.streetBySuburbId
+          },
+          geometry: r.streetGeometry
+        }))
+      };
+
+      map.addSource('streets-geometry', {
+        type: 'geojson',
+        data: featureCollection
+      });
+
+      map.addLayer({
+        id: 'streets-layer',
+        type: 'line',
+        source: 'streets-geometry',
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
         },
-        geometry: r.streetGeometry
-      }))
-    };
+        paint: {
+          'line-color': '#d10b0b',
+          'line-width': 3,
+          'line-opacity': 0.5
+        }
+      });
 
-    map.addSource('streets-geometries', {
-      type: 'geojson',
-      data: featureCollection
-    });
+      map.on('click', 'streets-layer', (e) => {
+        const feature = e.features?.[0]
 
-    map.addLayer({
-      id: 'streets-layer',
-      type: 'line',
-      source: 'streets-geometries',
-      paint: {
-        // 'line-color': '#d10b0b',
-        'line-color': '#d10b0b',
-        'line-width': 3,
-        'line-opacity': 0.5
-      }
-    });
-  } catch (err) {
-    console.error(err)
-  }
-});
+        if (!feature) return
+
+        const props = feature.properties
+
+        console.log('Clicked - street:', props.street, 'suburb:', props.suburb);
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  });
+})();
